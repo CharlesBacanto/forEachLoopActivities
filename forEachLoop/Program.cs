bb3ab945a2325e806bc5e2cd84f4928b500725e2
charlesdominique <charlesdominiquebacanto@gmail.com>
2024-11-21 11:14:52 +0800

    Add project files.

---
+++ forEachLoop/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {   
+        //Each names in an array
+        string[] userNames = { "Giyu", "Tanjiro", "Nezuko", "Inosuke", "Zenitsu" };
+        string findName = "Bacanto";
+        foreach(string name in userNames)
+        {
+            if (name.Equals(findName))
+            {
+                Console.WriteLine(name+" is found");
+                break;
+            }
+            else
+            {
+                Console.WriteLine("Name not found");
+            }
+           
+        }
+
+
+        //Each Letter in a Word
+        int charcount = 0;
+        string word = Console.ReadLine();
+        char letter = Convert.ToChar(Console.ReadLine());
+        foreach(char c in word)
+        {
+            if(c.Equals(letter))
+            {
+                charcount++;
+            }
+        }
+        Console.WriteLine(charcount);
+
+        
+
+       
+    }
+}
